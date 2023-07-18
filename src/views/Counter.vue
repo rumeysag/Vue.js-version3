09ef083b866169c4027a4de8ab701bc92e1b0606
rumeysag <rumeysasevenn@gmail.com>
2023-07-18 09:51:31 +0300

    V3- computed

--- src/views/Counter.vue
+++ src/views/Counter.vue
@@ -7,7 +7,16 @@
           <button @click="counter++" class="btn mr-2 btn-sm btn-primary">+</button>
           <button @click="counter--" class="btn btn-sm btn-danger">-</button>
         </div>
-        <div class="alert alert-warning mt-3"> {{ getCounterResult() }}</div>
+        <div class="alert alert-warning mt-3"> {{ getCounterResult }}</div>
+      </div>
+
+      <div class="col-2 card p-1">
+        <h3 class="text-center">{{counter2}}</h3>
+        <div class="d-flex justify-content-center align-items-center">
+          <button @click="counter2++" class="btn mr-2 btn-sm btn-primary">+</button>
+          <button @click="counter2--" class="btn btn-sm btn-danger">-</button>
+        </div>
+        <div class="alert alert-warning mt-3"> {{ getCounter2Result }}</div>
       </div>
     </div>
   </div>
@@ -17,14 +26,31 @@
 export default {
   data() {
     return {
-        counter: 0
+        counter: 0,
+        counter2: 0
     };
   },
   methods: {
+    // getCounterResult() {
+    //   console.log("Counter 1 çalıştı");
+    //      return this.counter > 5 ? 'BÜYÜK' : 'KÜÇÜK';
+    // },
+    // getCounter2Result() {
+    //   console.log("Counter 2 çalıştı");
+    //      return this.counter2 > 5 ? 'BÜYÜK' : 'KÜÇÜK';
+    // },
+  },
+  computed: {
     getCounterResult() {
+      console.log("Counter 1 çalıştı");
          return this.counter > 5 ? 'BÜYÜK' : 'KÜÇÜK';
     },
-  },
+    getCounter2Result() {
+      console.log("Counter 2 çalıştı");
+         return this.counter2 > 5 ? 'BÜYÜK' : 'KÜÇÜK';
+    },
+
+  }
 //   methods : {
 //     inc(){
 //         this.counter++;
